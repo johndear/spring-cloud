dbef6f48f235d1bc21eeff00691996a8ddf8a364
johndear <ls123456>
2016-12-07 22:56:47 +0800

    tomcat

--- compute-service/src/main/java/spring/cloud/ComputeServiceApplication.java
+++ compute-service/src/main/java/spring/cloud/ComputeServiceApplication.java
@@ -3,12 +3,13 @@ package spring.cloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("spring.cloud.dao")
-public class ComputeServiceApplication {
+public class ComputeServiceApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
 	}
--- compute-service/src/main/java/spring/cloud/web/ComputeController.java
+++ compute-service/src/main/java/spring/cloud/web/ComputeController.java
@@ -1,7 +1,6 @@
 package spring.cloud.web;
 
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -13,14 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComputeController {
-    private final Logger logger = Logger.getLogger(getClass());
     @Autowired
     private DiscoveryClient client;
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
     }
 }
