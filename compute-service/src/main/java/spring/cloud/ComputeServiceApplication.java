a51c4ce6e0816a809cd52e91089e0364466b86cc
liusu <wb-liusu.a@alibaba-inc.com>
2016-12-08 13:55:49 +0800

    +1

--- compute-service/src/main/java/spring/cloud/ComputeServiceApplication.java
+++ compute-service/src/main/java/spring/cloud/ComputeServiceApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableDiscoveryClient
 @SpringBootApplication
