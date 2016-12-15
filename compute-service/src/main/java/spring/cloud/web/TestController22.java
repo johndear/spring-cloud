3c561e3feb8171958c64bac9b4a42fc7f2e2dc4a
liusu <wb-liusu.a@alibaba-inc.com>
2016-12-15 15:22:28 +0800

    t

--- compute-service/src/main/java/spring/cloud/web/TestController22.java
+++ compute-service/src/main/java/spring/cloud/web/TestController22.java
@@ -18,6 +18,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,8 @@ import com.alibaba.fastjson.JSONObject;
 
 @RestController
 public class TestController22 {
+	
+	Logger logger = Logger.getLogger(TestController22.class);
 
 	ICountService countService;
 	
@@ -145,23 +148,65 @@ public class TestController22 {
 			}
 		}
 		
-		String callbackResult = "参数" + json + ", 回调结果：" + html;
+		String callbackResult = "参数" + json + ", 回调结果：" + 123;
 		System.out.println(callbackResult);
 		return callbackResult;
 	}
 	
 	@RequestMapping("/amount/get")
 	public String bb(String json){
+		logger.info("请求json参数：" + json);
+		
+		JSONObject jsonObj = null;
 		try {
 			if(StringUtils.isEmpty(json)){
 				return "json不允许为空！";
 			}
-			JSONObject jsonObj = JSONObject.parseObject(json);
-			long start = Calendar.getInstance().getTimeInMillis();
+			
+			jsonObj = JSONObject.parseObject(json);
+		} catch (Exception e) {
+			return "json格式有误,请仔细检查！参考格式如下：<br>"
+					+ "{<br>"
+					+ "&nbsp;&nbsp;callId: 'c0001',<br>"
+					+ "&nbsp;&nbsp;callFunctions: [{<br>"
+						+ "&nbsp;&nbsp;&nbsp;&nbsp;functionCallId: 'f0001',<br>"
+						+ "&nbsp;&nbsp;&nbsp;&nbsp;function: 'count',<br>"
+						+ "&nbsp;&nbsp;&nbsp;&nbsp;params: [{<br>"
+							+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;name: 'itemId',<br>"
+							+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;value: 'xxxx',<br>"
+						+ "&nbsp;&nbsp;&nbsp;&nbsp;}]<br>"
+					+ "&nbsp;&nbsp;}]<br>"
+				+ "}";
+		}
+		
+		// 异步调用
+		new MyThread(jsonObj).start();
+		
+		// 返回调用成功
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("code", 200);
+		resultMap.put("msg", "调用成功!");
+		return JSONObject.toJSONString(resultMap);
+	}
+
+}
+
+class MyThread extends Thread{
 	
+	JSONObject jsonObj;
+	
+	public MyThread(JSONObject jsonObj){
+		this.jsonObj = jsonObj;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			long start = Calendar.getInstance().getTimeInMillis();
+
 			Map<String, Object> resultMap = new HashMap<String, Object>();
 			resultMap.put(jsonObj.getString("callId"), (Map<String, Object>) new MyAsyncTaskExecutor().excute(jsonObj.getString("callFunctions")));
-	
+
 			long end = Calendar.getInstance().getTimeInMillis();
 			System.out.println("compute result:" + JSON.toJSONString(resultMap) + ", take timeInMillis:" + (end-start));
 			
@@ -183,37 +228,12 @@ public class TestController22 {
 				}
 			}
 			
-			String callbackResult = "参数" + json + ", 回调结果：" + html;
+			String callbackResult = "参数" + jsonObj.toJSONString() + ", 回调结果：" + html;
 			System.out.println(callbackResult);
-			return callbackResult;
-			
 		} catch (Exception e) {
-			return "json格式有误,请仔细检查！参考格式如下：<br>"
-					+ "{<br>"
-					+ "&nbsp;&nbsp;callId: 'c0001',<br>"
-					+ "&nbsp;&nbsp;callFunctions: [{<br>"
-						+ "&nbsp;&nbsp;&nbsp;&nbsp;functionCallId: 'f0001',<br>"
-						+ "&nbsp;&nbsp;&nbsp;&nbsp;function: 'count',<br>"
-						+ "&nbsp;&nbsp;&nbsp;&nbsp;params: [{<br>"
-							+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;name: 'itemId',<br>"
-							+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;value: 'xxxx',<br>"
-						+ "&nbsp;&nbsp;&nbsp;&nbsp;}]<br>"
-					+ "&nbsp;&nbsp;}]<br>"
-				+ "}";
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+				
 	}
-	
-	@RequestMapping("/amount/batch222222")
-	public void amount(String json){
-		json = "[{itemId:\"4001\",type:\"1\",datasource:\"0\",method:\"1\",startDate:\"2015\",endDate:\"2017-12-01\",function:\"count22\"},{itemId:\"4001\",type:\"1\",datasource:\"0\",method:\"1\",startDate:\"2015\",endDate:\"2017-12-01\",function:\"count\"}]";
-
-		long start = Calendar.getInstance().getTimeInMillis();
-
-		new MyAsyncTaskExecutor().excute(json);
-
-		long end = Calendar.getInstance().getTimeInMillis();
-		System.out.println("take time:" + (end-start));
-	}
-	
-
 }
